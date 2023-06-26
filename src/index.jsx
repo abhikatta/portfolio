692829c9bee1bc7b948a61d54bcf7ea246fe55f1
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-06-26 18:19:19 +0530

    ,

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
-import ReactDom from 'react-dom/client'
-import './index.css'
-
+import React, { useState } from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
 const Main = () => {
   const [counter, setCounter] = useState(0);
   const Increment = () => {
     setCounter(counter + 1);
-  }
+  };
   const Decrement = () => {
     setCounter(counter - 1);
-  }
+  };
   return (
     <>
       <h1>Abhinay-Katta</h1>
       {counter}
       <br></br>
-      <button className='buttons' onClick={Increment}>Inc</button>
-      <button className='buttons' onClick={Decrement}>Dec</button>
+      <button className="buttons" onClick={Increment}>
+        Inc
+      </button>
+      <button className="buttons" onClick={Decrement}>
+        Dec
+      </button>
     </>
   );
-}
+};
 
 const testt = ReactDom.createRoot(document.getElementById("root"));
 
@@ -28,4 +31,4 @@ testt.render(
   <>
     <Main />
   </>
-)
+);
