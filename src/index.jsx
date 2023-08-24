9a845058ae1050b5f80b6bf2df263cbd8187f7c4
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-08-24 17:26:02 +0530

    feat:updated to fix ReactDom.render method usage

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDom.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
