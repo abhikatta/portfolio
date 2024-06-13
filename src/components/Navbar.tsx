36f823e786cdf71fac058c501ff1ec75611a7030
abhikatta <abhinaykatta97@gmail.com>
2024-06-13 13:19:38 +0530

    navbar fixed for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = () => {
     <div>
       <div
         style={{ zIndex: 2 }}
-        className="fixed h-[3rem] hover:h-[5rem] transition-all duration-300 w-full top-4 flex justify-center items-center px-10 py-4 text-gray-300">
-        <div className="h-[3rem] hover:h-[5rem] transition-all duration-300 border-r-4 border-l-4 rounded-[1rem] w-[50%] backdrop-blur-lg flex flex-row justify-between items-center">
-          <div className="h-[3rem] hover:h-[5rem] transition-all duration-300">
+        className="fixed h-[3rem] hover:h-[5rem] lg:transition-all duration-300 w-full top-4 flex justify-center items-center px-10 py-4 text-gray-300">
+        <div className="h-[3rem] hover:h-[5rem] lg:transition-all duration-300 border-r-4 border-l-4 rounded-[1rem] w-[50%] backdrop-blur-lg lg:flex hidden flex-row justify-between items-center">
+          <div className="h-[3rem] hover:h-[5rem] lg:transition-all duration-300">
             <a className="lg:flex hidden " href="/portfolio">
               <img
                 src={Logo}
@@ -77,27 +77,27 @@ const Navbar = () => {
         <div
           onClick={handleclick}
           style={{ zIndex: 3 }}
-          className=" hover:cursor-pointer lg:hidden">
-          {!nav ? <FaBars size={40} /> : <FaTimes size={40} />}
+          className="lg:hover:cursor-pointer w-screen lg:hidden pt-20 flex justify-end">
+          {!nav ? <FaBars size={80} /> : <FaTimes size={80} />}
         </div>
         {/* on minimized/phone screen */}
         <ul
           className={
             !nav
               ? "hidden"
-              : "absolute top-0 left-0 w-full h-screen backdrop-blur-lg  flex flex-col justify-center items-center scale-125 "
+              : "absolute top-0 left-0 w-full h-screen backdrop-blur-lg  flex flex-col justify-center items-center  "
           }>
-          <li className="py-[4px] my-[25px] text-4xl hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
+          <li className="py-[4px] my-[25px] text-[4rem] hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
             <Link onClick={handleclick} to="home" smooth={true} duration={500}>
               Home
             </Link>
           </li>
-          <li className="py-[4px] my-[25px] text-4xl hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
+          <li className="py-[4px] my-[25px] text-[4rem] hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
             <Link onClick={handleclick} to="about" smooth={true} duration={500}>
               About
             </Link>
           </li>
-          <li className="py-[4px] my-[25px] text-4xl hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
+          <li className="py-[4px] my-[25px] text-[4rem] hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
             <Link
               onClick={handleclick}
               to="skills"
@@ -106,12 +106,12 @@ const Navbar = () => {
               Skills
             </Link>
           </li>
-          <li className="py-[4px] my-[25px] text-4xl hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
+          <li className="py-[4px] my-[25px] text-[4rem] hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
             <Link onClick={handleclick} to="work" smooth={true} duration={500}>
               Work
             </Link>
           </li>
-          <li className="py-[4px] my-[25px] text-4xl hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
+          <li className="py-[4px] my-[25px] text-[4rem] hover:text-black hover:bg-slate-50 duration-300 transition-colors rounded-md ">
             <Link
               onClick={handleclick}
               to="contact"
@@ -123,7 +123,7 @@ const Navbar = () => {
         </ul>
         {/* socials */}
       </div>
-      <div className="fixed lg:flex flex sm:hidden flex-col top-[35%] left-0">
+      <div className="fixed lg:flex hidden flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300">
             <a
