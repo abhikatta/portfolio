06b8e132acd6363f21deb6c327d300076dd29b7d
abhikatta <abhinaykatta97@gmail.com>
2024-11-01 15:18:52 +0530

    chore: remove firebase

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -1,18 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO:
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAhs6HhJj78z7St6uXlZZW6sipZmokxr_Q",
+//   authDomain: "portfolio-37085.firebaseapp.com",
+//   projectId: "portfolio-37085",
+//   storageBucket: "portfolio-37085.appspot.com",
+//   messagingSenderId: "751212889767",
+//   appId: "1:751212889767:web:8f651c6b3f82920c7bfd82",
+//   measurementId: "G-3199TQ53J6",
+// };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAhs6HhJj78z7St6uXlZZW6sipZmokxr_Q",
-  authDomain: "portfolio-37085.firebaseapp.com",
-  projectId: "portfolio-37085",
-  storageBucket: "portfolio-37085.appspot.com",
-  messagingSenderId: "751212889767",
-  appId: "1:751212889767:web:8f651c6b3f82920c7bfd82",
-  measurementId: "G-3199TQ53J6",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export { app, analytics };
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export { app, analytics };
--- src/app/firebase/initGoogleAnalytics.ts
+++ src/app/firebase/initGoogleAnalytics.ts
@@ -1,3 +1,3 @@
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-3199TQ53J6");
+// ReactGA.initialize("G-3199TQ53J6");
