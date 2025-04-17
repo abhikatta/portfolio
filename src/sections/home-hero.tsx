16253e28d82592fbf26775cd6cf2189b9dd79f6c
abhikatta <abhinaykatta97@gmail.com>
2025-04-17 16:18:19 +0530

    minor code cleanup

--- src/sections/home-hero.tsx
+++ src/sections/home-hero.tsx
@@ -23,28 +23,27 @@ const HomeHero = () => {
   };
 
   return (
-    <Container className="flex flex-row items-center justify-between pt-20">
-      <div>
-        <div className="mt-30 h-auto flex flex-col">
-          <p className="text-2xl lg:text-5xl font-syne mb-10">
-            hi, my name is{" "}
-          </p>
-          <motion.div id="header-title" className="flex flex-row">
-            {"Abhinay Katta".split("").map((letter, index) => (
-              <motion.span
-                variants={NameVariants}
-                initial="initial"
-                animate="animate"
-                custom={index}
-                key={index}
-                viewport={{ once: true }}
-                id="header-alphabet"
-                className="!font-boldonse uppercase text-4xl leading-20 lg:text-7xl 2xl:text-8xl font-black !text-primaryYellow">
-                {letter}
-              </motion.span>
-            ))}
-          </motion.div>
-        </div>
+    <Container className="flex flex-col items-start justify-between pt-20">
+      <div className="mt-30 h-auto flex flex-col">
+        <p className="text-2xl lg:text-5xl font-syne mb-10">hi, my name is </p>
+        <motion.div id="header-title" className="flex flex-row">
+          {"Abhinay Katta".split("").map((letter, index) => (
+            <motion.span
+              variants={NameVariants}
+              initial="initial"
+              animate="animate"
+              custom={index}
+              key={index}
+              viewport={{ once: true }}
+              id="header-alphabet"
+              className="!font-boldonse uppercase text-4xl leading-20 lg:text-7xl 2xl:text-8xl font-black !text-primaryYellow">
+              {letter}
+            </motion.span>
+          ))}
+        </motion.div>
+      </div>
+      <div className="mt-20">
+        <h2>I am a</h2>
       </div>
     </Container>
   );
