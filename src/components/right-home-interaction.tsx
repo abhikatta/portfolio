eba071ab4c8243c534fe056f0be8bc01e04eedb9
abhikatta <abhinaykatta97@gmail.com>
2025-04-27 21:16:05 +0530

    delett comments

--- src/components/right-home-interaction.tsx
+++ src/components/right-home-interaction.tsx
@@ -3,40 +3,19 @@
 import { cn } from "@/utils/cn";
 import { MotionDiv } from "@/utils/variant-props-spread";
 import { animate, motion } from "motion/react";
-import { memo, useState } from "react";
+import { memo } from "react";
 
 const idConstructor = (i: number, j: number) => `#${i}-${j}`;
-
-// const getColor = () =>
-//   ALL_COLORS[Math.floor(Math.random() * ALL_COLORS.length)];
-
-// const ALL_COLORS = [
-//   "var(--color-primaryYellow)",
-//   "var(--color-primaryBlue)",
-//   "var(--color-lightBlue)",
-//   "var(--color-accentYellow)",
-//   "var(--color-accentBlue)",
-//   "var(--color-viltrumRed)",
-//   "var(--color-white)",
-//   "var(--color-sinisterYellow)",
-// ];
 
 const GRID_SIZE = 10;
 
 const RightHomeInteraction = memo(() => {
-  const [values, setValues] = useState({
-    i: 0,
-    j: 0,
-  });
-
   const animateGrid = (i: number, j: number, type: "leave" | "enter") => {
-    setValues({ i, j });
     const directions = [
-      { dx: 0, dy: -1 }, // Up
-      { dx: 0, dy: 1 }, // Down
-      { dx: -1, dy: 0 }, // Left
-      { dx: 1, dy: 0 }, // Right
-
+      { dx: 0, dy: -1 },
+      { dx: 0, dy: 1 },
+      { dx: -1, dy: 0 },
+      { dx: 1, dy: 0 },
       { dx: 1, dy: 1 },
       { dx: 1, dy: -1 },
       { dx: -1, dy: -1 },
@@ -75,24 +54,21 @@ const RightHomeInteraction = memo(() => {
 
   // TODO: make this stagger like a ripple effect or something like the particles js thing that pushes away neighboring elements
   return (
-    <>
-      <MotionDiv className="relative z-10 grid grid-cols-10">
-        {Array.from({ length: GRID_SIZE }).map((_, indexj) =>
-          Array.from({ length: GRID_SIZE }).map((_, indexi) => (
-            <motion.div
-              id={idConstructor(indexi, indexj)}
-              onMouseOver={() => animateGrid(indexi, indexj, "enter")}
-              onMouseLeave={() => animateGrid(indexi, indexj, "leave")}
-              className={cn(
-                "bg-accentYellow size-6 cursor-pointer rounded-[6px] will-change-transform",
-              )}
-              key={`${indexi}-${indexj}`}
-            />
-          )),
-        )}
-      </MotionDiv>
-      <pre className="mt-10">{JSON.stringify(values)}</pre>
-    </>
+    <MotionDiv className="relative z-10 grid grid-cols-10">
+      {Array.from({ length: GRID_SIZE }).map((_, indexj) =>
+        Array.from({ length: GRID_SIZE }).map((_, indexi) => (
+          <motion.div
+            id={idConstructor(indexi, indexj)}
+            onMouseOver={() => animateGrid(indexi, indexj, "enter")}
+            onMouseLeave={() => animateGrid(indexi, indexj, "leave")}
+            className={cn(
+              "bg-accentYellow size-6 cursor-pointer rounded-[6px] will-change-transform",
+            )}
+            key={`${indexi}-${indexj}`}
+          />
+        )),
+      )}
+    </MotionDiv>
   );
 });
 
